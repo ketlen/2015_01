80c9b1bdae5b51d9eb2e4a7d2bb8f5ac625936ab
LmarioLmario <luizmario_10@hotmail.com>
2015-06-10 15:40:21 -0400

    Atualizando android

--- Android/EncontreEmManaus/gen/br/com/encontreemmanaus/R.java
+++ Android/EncontreEmManaus/gen/br/com/encontreemmanaus/R.java
@@ -23,36 +23,51 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_teatro=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
-        public static final int btrestaurante=0x7f080006;
-        public static final int btrestaurantepeixaria=0x7f080013;
+        public static final int action_settings=0x7f080022;
+        public static final int btCadastar=0x7f080007;
+        public static final int btGravar=0x7f080011;
+        public static final int btMenuPrincipal=0x7f080021;
+        public static final int btRestaurante=0x7f080005;
+        public static final int btRestaurantePeixaria=0x7f080020;
+        public static final int btVoltar=0x7f080013;
+        public static final int btVoltar3=0x7f08001a;
         public static final int btshopping=0x7f080001;
-        public static final int btvoltar=0x7f080014;
-        public static final int btvoltarconsulta=0x7f080011;
-        public static final int button1=0x7f080004;
+        public static final int btvoltar2=0x7f080012;
+        public static final int button1=0x7f080003;
         public static final int button2=0x7f080000;
-        public static final int button3=0x7f080005;
-        public static final int button4=0x7f080012;
-        public static final int button5=0x7f080003;
-        public static final int button6=0x7f080002;
-        public static final int editText1=0x7f08000a;
-        public static final int editText2=0x7f08000c;
-        public static final int editText3=0x7f08000e;
-        public static final int editText4=0x7f080010;
-        public static final int searchView1=0x7f080007;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f08001f;
+        public static final int button5=0x7f080002;
+        public static final int categoria=0x7f08000a;
+        public static final int endereco=0x7f08000c;
+        public static final int horario=0x7f080010;
+        public static final int searchView1=0x7f080006;
+        public static final int telefone=0x7f08000e;
         public static final int textView1=0x7f080008;
+        public static final int textView10=0x7f080018;
+        public static final int textView11=0x7f080019;
         public static final int textView2=0x7f080009;
         public static final int textView3=0x7f08000b;
         public static final int textView4=0x7f08000d;
         public static final int textView5=0x7f08000f;
+        public static final int textView6=0x7f080014;
+        public static final int textView7=0x7f080015;
+        public static final int textView8=0x7f080016;
+        public static final int textView9=0x7f080017;
+        public static final int tvCategoria=0x7f08001c;
+        public static final int tvEndereco=0x7f08001b;
+        public static final int tvHorario=0x7f08001e;
+        public static final int tvTelefone=0x7f08001d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int cadastro=0x7f030001;
         public static final int exibir_local=0x7f030002;
-        public static final int listar_locais=0x7f030003;
+        public static final int exibir_local2=0x7f030003;
+        public static final int listar_locais=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Android/EncontreEmManaus/src/br/com/encontreemmanaus/MainActivity.java
+++ Android/EncontreEmManaus/src/br/com/encontreemmanaus/MainActivity.java
@@ -1,37 +1,42 @@
 package br.com.encontreemmanaus;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	//Button btRestaurante;
+    Registro objRegistro;
+    EditText etCategoria, etEndereco, etTelefone, etHorario;
+    int contadorRegistro=0;
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
 		chamaMenuPrincipal();
 		
-				
+						
 		}
-	public void chamaRestaurantes(){
+	public void chamaRestaurante(){
 		setContentView(R.layout.listar_locais);
-		
-	   Button	btVoltar = (Button) findViewById(R.id.btvoltar);
-        btVoltar.setOnClickListener(new View.OnClickListener() {
+		Button btMenuPrincipal = (Button) findViewById(R.id.btMenuPrincipal);
+        btMenuPrincipal.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				chamaVoltar();
+			chamaMenuPrincipal();
 				
 			}
 		});
-        Button	btRestaurantePeixaria = (Button) findViewById(R.id.btrestaurantepeixaria);
-        btRestaurantePeixaria.setOnClickListener(new View.OnClickListener() {
+		Button btRestaurantePeixaria = (Button) findViewById(R.id.btRestaurantePeixaria);
+		btRestaurantePeixaria.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -39,50 +44,166 @@ public class MainActivity extends Activity {
 				
 			}
 		});
-		
-	}
 	
-	public void chamaVoltar(){
+	    }
+	public void chamaMenuPrincipal(){
+		
 		setContentView(R.layout.activity_main);
-		
-		
-	
-		
+		Button btRestaurante = (Button) findViewById(R.id.btRestaurante);
+	    btRestaurante.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chamaRestaurante();  //chama a tela de listar ao clicar
+				
+			}
+		}); 
+	    
+	    Button btCadastar = (Button) findViewById(R.id.btCadastar);
+	    btCadastar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chamaCadastro();
+				
+			}
+		});
+	    
+	    Button btshopping =(Button) findViewById(R.id.btshopping);
+	    btshopping.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chamaShopping();
+				
+			}
+		});
 		
 	}
 	public void chamaRestaurantePeixaria(){
+		
 		setContentView(R.layout.exibir_local);
-		
-		Button	btVoltarConsulta = (Button) findViewById(R.id.btvoltarconsulta);
-        btVoltarConsulta.setOnClickListener(new View.OnClickListener() {
+		Button btVoltar = (Button) findViewById(R.id.btVoltar);
+		btVoltar.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				chamaVoltarConsulta();
+				chamaRestaurante();
 				
 			}
 		});
-	}
-	public void chamaVoltarConsulta(){
-		setContentView(R.layout.activity_main);
+		
 		
 	}
-	public void chamaMenuPrincipal(){
-		setContentView(R.layout.activity_main);
-	Button	btRestaurante = (Button) findViewById(R.id.btrestaurante);
-		btRestaurante.setOnClickListener(new View.OnClickListener() {
+	
+	public void chamaCadastro(){
+		setContentView(R.layout.cadastro);
+		Button btVoltar2 = (Button) findViewById(R.id.btvoltar2);
+		btVoltar2.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
+				chamaMenuPrincipal();
 				
-				chamaRestaurantes(); // chama a tela de cadastro ao clicar
-								
 			}
 		});
+		
+		Button btGravar = (Button) findViewById(R.id.btGravar);
+		btGravar.setOnClickListener(new View.OnClickListener() {
 			
+			@Override
+			public void onClick(View v) {
+				try{
+				objRegistro = new Registro();
+				etCategoria = (EditText) findViewById(R.id.categoria);
+				etEndereco = (EditText) findViewById(R.id.endereco);
+				etTelefone = (EditText) findViewById(R.id.telefone);
+				etHorario = (EditText) findViewById(R.id.horario);
+				
+				//gravar
+				objRegistro.categoria = etCategoria.getText().toString();
+				objRegistro.endereco = etEndereco.getText().toString();
+				objRegistro.telefone = etTelefone.getText().toString();
+				objRegistro.horario = etHorario.getText().toString();
+				contadorRegistro++;
+				mensagemExibir("Gravação","Dados gravados com sucesso");
+				chamaMenuPrincipal();
+				
+			}
+				catch(Exception erro)
+				{
+				mensagemExibir("Erro","Deu erro ao gravar"+erro); 
+						
+		}
+			}
+		});
+	}
+	public  void mensagemExibir(String titulo, String texto)
+	{
+		AlertDialog.Builder mensagem = new AlertDialog.Builder(MainActivity.this);
+		mensagem.setTitle(titulo);
+		mensagem.setMessage(texto);
+		mensagem.setNeutralButton("Ok", null);
+		mensagem.show();
+	}
+	public void chamaShopping(){
+		if (contadorRegistro == 0)
+		{
+			mensagemExibir("Aviso","Não tem Registo Gravados");
+			
+		//	chamaMenuPrincipal();
+			return;
+			
+		}
+		
+		setContentView(R.layout.exibir_local2);
+		
+		
+		TextView tvCategoria = (TextView) findViewById(R.id.tvCategoria);
+		TextView tvEndereco = (TextView) findViewById(R.id.tvEndereco);
+		TextView tvTelefone = (TextView) findViewById(R.id.tvTelefone);
+		TextView tvHorario = (TextView) findViewById(R.id.tvHorario);
+		
+		tvCategoria.setText(objRegistro.categoria);
+		tvEndereco.setText(objRegistro.endereco);
+		tvTelefone.setText(objRegistro.telefone);
+		tvHorario.setText(objRegistro.horario);
+		
+		Button btVoltar3 = (Button) findViewById(R.id.btVoltar3);
+		btVoltar3.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chamaMenuPrincipal();
+				
+			}
+		});
+		
+		
+}
+
+}
+
+		
+
+// 3711
+//3726 persistencia
+
+
+
+
+
+
+
+
+
+
+
+
+
 				
 		
-	}
+	/*
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -100,6 +221,5 @@ public class MainActivity extends Activity {
 		if (id == R.id.action_settings) {
 			return true;
 		}
-		return super.onOptionsItemSelected(item);
-	}
-}
+		return super.onOptionsItemSelected(item);*/
+	
